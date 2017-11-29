5f5744877430e9316825cf838db0dde9dc19402f
Gabriel Liévano <gabriell@google.com>
2017-11-28 16:53:20 -0800

    We can now parse chapters and obtain each title and link

--- main.go
+++ main.go
@@ -17,6 +17,75 @@ func printUsage() {
 	fmt.Println("  kindle-email  (optional) Kindle email to send ebook to")
 }
 
+func getHref(node *html.Node) string {
+	for _, attr := range node.Attr {
+		if attr.Key == "href" {
+			return attr.Val
+		}
+	}
+	return ""
+}
+
+func nodeIsOfClass(node *html.Node, className string) bool {
+	for _, attr := range node.Attr {
+		if attr.Key == "class" {
+			if attr.Val == className {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// Gets FIRST child in parent node with class.
+func getChildOfClass(parent *html.Node, tag string, className string) *html.Node {
+	for c := parent.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type == html.ElementNode && c.Data == tag {
+			if nodeIsOfClass(c, className) {
+				return c
+			}
+		}
+	}
+	return nil
+}
+
+// Gets FIRST child in parent node with tag.
+func getChildWithTag(parent *html.Node, tag string) *html.Node {
+	for c := parent.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type == html.ElementNode && c.Data == tag {
+			return c
+		}
+	}
+	return nil
+}
+
+func parseBookHtmlNode(node *html.Node) {
+	if node.Type == html.ElementNode && node.Data == "div" {
+		if nodeIsOfClass(node, "mu_contain") {
+			// Check if mu_h1 class element exists. Which means is not the complete chapter list.
+			if getChildOfClass(node, "div", "mu_h1") == nil {
+				list := getChildOfClass(node, "ul", "mulu_list")
+				if list != nil {
+					for li := list.FirstChild; li != nil; li = li.NextSibling {
+						if li.Type == html.ElementNode {
+							link := getChildWithTag(li, "a")
+							for text := link.FirstChild; text != nil; text = text.NextSibling {
+								if text.Type == html.TextNode {
+									fmt.Println(text.Data)
+								}
+							}
+							fmt.Println(getHref(link))
+						}
+					}
+				}
+			}
+		}
+	}
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		parseBookHtmlNode(c)
+	}
+}
+
 func processBookUrl(url string) {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -24,18 +93,12 @@ func processBookUrl(url string) {
 	}
 	defer resp.Body.Close()
 
-	tokenizer := html.NewTokenizer(resp.Body)
-	for {
-		tt := tokenizer.Next()
-		switch {
-		case tt == html.ErrorToken:
-			// End of document.
-			return
-		case tt == html.StartTagToken:
-			token := tokenizer.Token()
-			fmt.Println(token.Data)
-		}
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		panic(err)
 	}
+
+	parseBookHtmlNode(doc)
 }
 
 // Download and parse chapter URLs.
